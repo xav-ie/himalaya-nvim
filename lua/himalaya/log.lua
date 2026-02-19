f3b41db79ff46d2a72b77f0e1d9c06483f118d9d
Xavier Ruiz <github@xav.ie>
2026-02-19 11:18:20 -0500

    debug: intercept vim.notify and log.err with stack traces
    Adds two tracing hooks:
    - vim.notify interceptor: catches any notification containing "parse JSON"
      regardless of source, with full stack traceback
    - log.err wrapper: traces every error notification with stack traceback
    
    Both write to /tmp/himalaya-debug.log.

--- lua/himalaya/log.lua
+++ lua/himalaya/log.lua
@@ -1,5 +1,24 @@
 local M = {}
 
+--- Write a line to /tmp/himalaya-debug.log (temporary debugging aid).
+local function trace(msg)
+  local f = io.open('/tmp/himalaya-debug.log', 'a')
+  if f then
+    f:write(os.date('%H:%M:%S') .. ' ' .. msg .. '\n')
+    f:close()
+  end
+end
+
+-- Intercept vim.notify to catch any "parse JSON" error regardless of source.
+local _real_notify = vim.notify
+vim.notify = function(msg, level, ...)
+  if type(msg) == 'string' and msg:find('parse JSON') then
+    trace('NOTIFY level=' .. tostring(level) .. ' msg=' .. msg:sub(1, 200)
+      .. '\n  ' .. debug.traceback('', 2):gsub('\n', '\n  '))
+  end
+  return _real_notify(msg, level, ...)
+end
+
 function M.info(msg)
   vim.notify(msg, vim.log.levels.INFO)
 end
@@ -9,6 +28,7 @@ function M.warn(msg)
 end
 
 function M.err(msg)
+  trace('LOG_ERR msg=' .. msg:sub(1, 200) .. '\n  ' .. debug.traceback('', 2):gsub('\n', '\n  '))
   vim.notify(msg, vim.log.levels.ERROR)
 end
 
